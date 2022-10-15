ac3b71a52c2d29b53a5ef72f9348fe6bf63fe5b2
gespel <heimbrodt@uni-potsdam.de>
2022-10-15 22:57:14 +0200

    asd

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut cap = Device::lookup().unwrap().expect("ERROR").open().unwrap();
 
     while let Ok(packet) = cap.next_packet() {
-        println!("received packet! {:?}", packet);
+        //println!("received packet! {:?}", packet);
+        println!("{:?}", packet.to_ascii_lowercase());
     }
 }
